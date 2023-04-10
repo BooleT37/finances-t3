accac2fee96a4bb2dfdc711c5e1158423ff3403d
Alexey Levin <boolet37@gmail.com>
2023-04-10 19:24:33 +0200

    feat(category): readable error message if cannot remove category

--- src/components/categories/RemoveButtonRenderer.tsx
+++ src/components/categories/RemoveButtonRenderer.tsx
@@ -1,4 +1,5 @@
 import { DeleteOutlined } from "@ant-design/icons";
+import { TRPCClientError } from "@trpc/client";
 import { Button, Modal, Tooltip } from "antd";
 import React from "react";
 import categoriesStore from "~/stores/categoriesStore";
@@ -13,7 +14,24 @@ const RemoveButtonRenderer: React.FC<Props> = ({ id, disabled }) => {
   const handleClick = React.useCallback(() => {
     Modal.confirm({
       content: "Вы уверены, что хотите удалить эту категорию?",
-      onOk: () => categoriesStore.deleteCategory(id),
+      onOk: async () => {
+        try {
+          await categoriesStore.deleteCategory(id);
+        } catch (e) {
+          if (e instanceof TRPCClientError) {
+            Modal.error({
+              title: "Ошибка при удалении категории",
+              content:
+                "Возможно, вы пытаетесь удалить " +
+                "категорию, к которой уже привязаны другие сущности " +
+                "(расходы, подписки, прогнозы). Удалите эти сущности прежде, " +
+                "чем удалять категорию.",
+            });
+          } else {
+            throw e;
+          }
+        }
+      },
     });
   }, [id]);
 
