cc12c6f2500c5df666e9a0dd6afd50d5239133c8
Alexey Levin <BooleT37@gmail.com>
2023-04-12 18:17:49 +0200

    fix(data-fetcher): Display error if couldn't load the data

--- src/components/DataFetcher/useDataStores.ts
+++ src/components/DataFetcher/useDataStores.ts
@@ -1,3 +1,4 @@
+import { Modal } from "antd";
 import { useEffect, useState } from "react";
 import type { DataLoader } from "~/stores/DataLoader";
 import type { Stores } from "./types";
@@ -12,24 +13,34 @@ export function useDataStores(stores: Stores) {
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     (async () => {
-      const datum = await Promise.all(
-        storesList.map(async (store) => {
-          if (store.dataLoaded || store.dataLoading) {
-            return;
+      try {
+        const datum = await Promise.all(
+          storesList.map(async (store) => {
+            if (store.dataLoaded || store.dataLoading) {
+              return;
+            }
+            store.setDataLoading(true);
+            const data = await store.loadData();
+            store.setDataLoaded(true);
+            store.setDataLoading(false);
+            return data;
+          })
+        );
+        storesList.map((store, index) => {
+          if (datum[index]) {
+            store.init(datum[index]);
           }
-          store.setDataLoading(true);
-          const data = await store.loadData();
-          store.setDataLoaded(true);
-          store.setDataLoading(false);
-          return data;
-        })
-      );
-      storesList.map((store, index) => {
-        if (datum[index]) {
-          store.init(datum[index]);
-        }
-      });
-      setDataLoaded(storesList.every((store) => store.dataLoaded));
+        });
+        setDataLoaded(storesList.every((store) => store.dataLoaded));
+      } catch (e) {
+        Modal.error({
+          title: "Ошибка при загрузке данных",
+          content: `При загрузке данных происошла ошибка. Вероятнее всего, привышено время ожидания ответа. Перезагрузите страницу\n\nОшибка: ${
+            (e as Error).message
+          }`,
+        });
+        throw e;
+      }
     })();
   }, [storesList]);
   return { dataLoaded };
