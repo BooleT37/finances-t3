c2ac043d23a83c8ce71b3f1263c23493fa5286ed
Alexey Levin <BooleT37@gmail.com>
2023-04-15 09:23:56 +0200

    chore(db): update default user with prod user id

---
+++ prisma/migrations/20230415073607_change_default_user_to_prod/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "Category" ALTER COLUMN "userId" SET DEFAULT 'clg6kpbtn0000mr081ntz0f8i';
+
+-- AlterTable
+ALTER TABLE "Expense" ALTER COLUMN "userId" SET DEFAULT 'clg6kpbtn0000mr081ntz0f8i';
+
+-- AlterTable
+ALTER TABLE "Forecast" ALTER COLUMN "userId" SET DEFAULT 'clg6kpbtn0000mr081ntz0f8i';
+
+-- AlterTable
+ALTER TABLE "SavingSpending" ALTER COLUMN "userId" SET DEFAULT 'clg6kpbtn0000mr081ntz0f8i';
+
+-- AlterTable
+ALTER TABLE "Source" ALTER COLUMN "userId" SET DEFAULT 'clg6kpbtn0000mr081ntz0f8i';
+
+-- AlterTable
+ALTER TABLE "Subscription" ALTER COLUMN "userId" SET DEFAULT 'clg6kpbtn0000mr081ntz0f8i';
