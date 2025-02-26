5819c883dc9789db88a0be9d5ff13911f743db92
Alexey Levin <BooleT37@gmail.com>
2025-02-26 19:40:22 +0100

    feat(comparison-chart): make chart horizontal

--- src/features/statistics/components/ComparisonChart/getOptions.ts
+++ src/features/statistics/components/ComparisonChart/getOptions.ts
@@ -1,66 +1,42 @@
-import {
-  type AgCartesianChartOptions,
-  type AgSeriesTooltipRendererParams,
-  type AgTooltipRendererResult,
-} from "ag-charts-community";
+import { type AgCartesianChartOptions } from "ag-charts-community";
 import { costToString } from "~/utils/costUtils";
 import type { ComparisonData } from "./models";
-import type { ComparisonDataCategory } from "./models/comparisonData";
-
-const getTooltip = (yKey: "period1" | "period2") => ({
-  renderer: ({
-    datum,
-    title,
-  }: AgSeriesTooltipRendererParams<ComparisonDataCategory>): AgTooltipRendererResult => {
-    const value = datum[yKey];
-
-    const cost = datum.isIncome ? value : -value;
-    return {
-      title,
-      data: [
-        {
-          label: "Всего:",
-          value: costToString(cost),
-        },
-      ],
-    };
-  },
-});
 
 const getOptions = (
   period1: string,
   period2: string,
   data: ComparisonData
 ): AgCartesianChartOptions => ({
+  height: 600,
   data,
   series: [
     {
       type: "bar",
+      direction: "horizontal",
       xKey: "category",
       yKey: "period1",
       yName: period1,
-      tooltip: getTooltip("period1"),
     },
     {
       type: "bar",
+      direction: "horizontal",
       xKey: "category",
       yKey: "period2",
       yName: period2,
-      tooltip: getTooltip("period2"),
     },
   ],
   axes: [
     {
-      type: "category",
-      position: "bottom",
-    },
-    {
       type: "number",
-      position: "left",
+      position: "bottom",
       label: {
         formatter: (params: { value: string }) =>
           costToString(parseFloat(params.value)),
       },
+    },
+    {
+      type: "category",
+      position: "left",
     },
   ],
 });
